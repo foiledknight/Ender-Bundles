a27474eee19fa4d59178b408b86f757019f8567e
tinfoilknight-macos <dino.daniel345@gmail.com>
2026-03-11 13:20:56 -0700

    ported to 1.21.3

--- src/main/java/womp/tinfoilknight/ender_bundles/EnderBundles.java
+++ src/main/java/womp/tinfoilknight/ender_bundles/EnderBundles.java
@@ -66,7 +66,7 @@ public class EnderBundles
     }
 
 
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
